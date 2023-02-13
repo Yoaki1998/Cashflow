b0a488848b1ff3631ed30eb05c72196270977977
Yoaki <yoaki@DESKTOP-D3C0IEM.localdomain>
2023-02-13 14:55:28 +0100

    add moves controller + routes

---
+++ app/controllers/moves_controller.rb
@@ -0,0 +1,22 @@
+class MovesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+end

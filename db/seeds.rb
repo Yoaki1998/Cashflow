3ecf305be4f048c547deb583c5b31ca882bc3e45
Yoaki <yoaki@DESKTOP-D3C0IEM.localdomain>
2023-02-14 19:17:50 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning Database..."
+Move.destroy_all
+
+puts "Creating moves..."
+
+Move.create(name:"Salaire dev", amount:3200, version:"salaire", user_id:1)
+Move.create(name:"Sizebusiness", amount:1200, version:"salaire", user_id:1)
+Move.create(name:"Course", amount:-150, version:"depense", user_id:1)
+Move.create(name:"Essence", amount:-320, version:"depense", user_id:1)
+Move.create(name:"Réparation", amount:-300, version:"depense", user_id:1)
+Move.create(name:"Loyer", amount:-1200, version:"depense", user_id:1)
+Move.create(name:"Epargne", amount:500, version:"epargne", user_id:1)
+Move.create(name:"Salaire dev", amount:2600, version:"salaire", user_id:2)
+Move.create(name:"Sizebusiness", amount:700, version:"salaire", user_id:2)
+Move.create(name:"Course", amount:-150, version:"depense", user_id:2)
+Move.create(name:"Essence", amount:-320, version:"depense", user_id:2)
+Move.create(name:"Loisir", amount:-80, version:"depense", user_id:2)
+Move.create(name:"Loyer", amount:-700, version:"depense", user_id:2)
+Move.create(name:"Epargne", amount:500, version:"epargne", user_id:2)
+
+puts "Finished!"
